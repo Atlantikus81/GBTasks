79ec49cd0e6069c535e818f3e17c6abd847d8979
Alexey Dudnikov <dudnikov-81@mail.ru>
2022-12-20 00:30:43 +0300

    добавили задачу 2

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+if (a>b) WriteLine ($"Число {a} больше, чем число {b}");
+else
+{
+   WriteLine ($"Число {b} больше, чем число {a}");
+}
+//int b = Convert ToInt32(ReadLine())
+
