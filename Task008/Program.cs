f4721bcfd5adb4f9a7ba4813d7eb525d9422735a
Alexey Dudnikov <dudnikov-81@mail.ru>
2022-12-20 04:39:36 +0300

    добавили задачу 8

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите любое положительное число: ");
+int N = int.Parse(ReadLine()!);
+int a = 1;
+
+while (a<=N)
+{
+    if (a%2==0)
+    {
+        Write($"{a} ");
+        a=a+1;
+    }
+    else a=a+1;
+}
