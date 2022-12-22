d6026751be591db13702ca394c7f78351ca11bd0
Alexey Dudnikov <dudnikov-81@mail.ru>
2022-12-22 23:55:06 +0300

    добавили задачу 10

---
+++ Task010/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа вывода второй цифры трёхзначного числа
+
+using static System.Console;
+
+int number = new Random().Next(100,1000);
+WriteLine(number);
+
+WriteLine($"Вторая цифра этого числа: {number/10%10}");
+
+// WriteLine("Введите любое трёхзначное число: ");
+// int a = int.Parse(ReadLine()!);
+
+// if (a>99&&a<1000)
+// {
+//     Write($"Вторая цифра этого числа: {a=a/10%10}");
+// }
+// else
+// {
+//    Write("Введено некорректное число");
+// }
