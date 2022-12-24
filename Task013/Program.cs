f4d18337addf4b0713c5ce91434ced586453fd5a
Alexey Dudnikov <dudnikov-81@mail.ru>
2022-12-24 20:57:31 +0300

    добавили задачу 13

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿// Программа вывода третьей цифры заданного числа
+
+using static System.Console;
+
+WriteLine("Введите любое число: ");
+int a = int.Parse(ReadLine()!);
+
+if (a<100)
+{
+    WriteLine("Третьей цифры нет");
+    return;
+}
+if (a>99&&a<999)
+{
+    WriteLine($"{a%10}");
+    return;
+}
+    while (a>999)
+    {
+        a=a/10;
+    }
+    WriteLine($"{a%10}");
