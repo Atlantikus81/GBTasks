83d171d493b75b3170de90522dc6d92cfab6915c
Alexey Dudnikov <dudnikov-81@mail.ru>
2022-12-26 22:39:12 +0300

    добавили задачу 23

---
+++ Task023/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа вывода кубов от 1 до заданного числа
+
+using static System.Console;
+
+WriteLine ("Введите любое число:");
+
+int N = int.Parse(ReadLine()!);
+int a = 1;
+
+WriteLine ($"Список кубов чисел от 1 до {N}:");
+
+while (a<=N)
+{
+    Write ($"{Math.Pow(a,3)}, ");
+    a++;
+}
