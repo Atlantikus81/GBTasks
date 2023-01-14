c0457d9a4ed50f15c6d30e25353e4de2fa0dc69f
Alexey Dudnikov <dudnikov-81@mail.ru>
2023-01-14 20:31:30 +0300

    добавили задачу  25

---
+++ Task025/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа по возведению одного числа в степень другого
+
+using static System.Console;
+
+Write ("Введите число A: ");
+int A = int.Parse(ReadLine()!);
+
+Write ("Введите число B: ");
+int B = int.Parse(ReadLine()!);
+
+int exp = EXP (A, B);
+
+WriteLine ($"Значение числа {A} в степени {B} равно: {exp}");
+
+int EXP (int a, int b)
+{
+    int result = a;
+    int n = 1;
+    while (n < b)
+    {
+        result = result * a;
+        n++;
+    }
+    return result;
+}
