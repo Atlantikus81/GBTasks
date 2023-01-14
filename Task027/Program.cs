e1f52e7ef249d66f1d1183cf129a2a68fdfafd7d
Alexey Dudnikov <dudnikov-81@mail.ru>
2023-01-14 21:03:24 +0300

    добавили задачу 27

---
+++ Task027/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27:
+// Напишите программу, реализующую метод,
+// который принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using static System.Console;
+
+Write("Введите любое целое число: ");
+int a = int.Parse(ReadLine()!);
+
+int sum = SumNumbers (a);
+
+WriteLine ($"Сумма цифр числа {a} равна: {sum}");
+
+int SumNumbers (int A)
+{
+    int result = 0;
+    int A1 = 0;
+    while (A > 0)
+    {
+        A1 = A%10;
+        result = result + A1;
+        A = A / 10;
+    }
+    return result;
+}
